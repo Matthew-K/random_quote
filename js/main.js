1f1b7aa083872ee16b37ccb288cc85f073591d4b
Matthew Kolodziejczak <makolodzi@gmail.com>
2016-05-10 12:36:28 -0400

    Have button generate new quote data

--- js/main.js
+++ js/main.js
@@ -1,21 +1,64 @@
 $(document).ready(function() {
 
-var getQuote = function(){
-	$.ajax({
-	    headers: {
-	      "X-Mashape-Key": "rN5svBBLrXmshBjyfGqbrToUTAmMp1O6xFujsnuNkyvK1rG049",
-	      "Content-Type": "application/x-www-form-urlencoded",
-	      "Accept": "application/json"
-	    },
-		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-		success: function(info){
-			console.log(info);
-		}
-	});
+/*==========   MODEL   ==========*/
+
+// Will contain data once controller.getQuote is called. Example shown within {}
+quoteData = {
+  // "quote": "Once you eliminate the impossible, whatever remains, no matter how improbable, must be the truth.",
+  // "author": "Sherlock Holmes",
+  // "category": "Famous",
+  // "cat": ""
 };
 
-$("#generate").on("click", function(){
-	getQuote();
-});
+
+
+/*==========   CONTROLLER   ==========*/
+controller = {
+
+	init: function(){
+		controller.getQuote();
+		view.generate();
+	},
+
+	setQuoteData: function(info){
+		quoteData = info;
+	},
+
+	currentData: quoteData,
+
+	getQuote: function(){
+		$.ajax({
+		    headers: {
+		      "X-Mashape-Key": "rN5svBBLrXmshBjyfGqbrToUTAmMp1O6xFujsnuNkyvK1rG049",
+		      "Content-Type": "application/x-www-form-urlencoded",
+		      "Accept": "application/json"
+		    },
+			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+			success: function(info){
+				console.log(info);
+				controller.setQuoteData(info);
+			}
+		});
+	}
+};
+
+
+
+/*==========   View   ==========*/
+
+var view = {
+	generate: function(){
+		$("#generate").on("click", function(){
+			controller.getQuote();
+		});
+	}	
+
+
+
+};
+
+
+
+controller.init();
 
 }); // end of document.ready
